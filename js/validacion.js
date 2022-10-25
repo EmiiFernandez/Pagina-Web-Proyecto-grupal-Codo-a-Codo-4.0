6658c38bce0d5704098d1c1acdd49f5fa82baf21
EmiiFernandez <e.fernandezmurgia@gmail.com>
2022-10-24 23:04:57 -0300

    corrigiendo desplegable

--- js/validacion.js
+++ js/validacion.js
@@ -1,11 +1,11 @@
-var nombre = document.getElementById('nombre');
-var correoelectronico = document.getElementById('correo electronico');
-var error = document.getElementById('error');
+let nombre = document.getElementById('nombre');
+let correoelectronico = document.getElementById('correo electronico');
+let error = document.getElementById('error');
 error.style.color = 'red';
 
 function  enviarFormulario(){
    console.log('Enviando formulario...'); 
- var mensajesError = [];
+ let mensajesError = [];
 
  if(nombre.value === null || exampleFormControlInput1.value === ''){
     mensajesError.push('Ingresa tu nombre');
